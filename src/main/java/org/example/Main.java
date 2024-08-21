f3fa5e86d54c8cff2c9105409aa6e8fb2ee88fb8
shoccho <sfb.shoccho@gmail.com>
2024-08-22 00:46:03 +0600

    fixed elements not appearing until resizing

--- src/main/java/org/example/Main.java
+++ src/main/java/org/example/Main.java
@@ -1,7 +1,13 @@
 package org.example;
 
+import javax.swing.*;
+
 public class Main {
     public static void main(String[] args) {
-        apiForm form = new apiForm();
+        SwingUtilities.invokeLater(new Runnable() {
+            public void run() {
+                new apiForm();
+            }
+        });
     }
 }
