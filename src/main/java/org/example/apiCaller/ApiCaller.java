dbe31942db30a3908ff78a7acf2e67de51d5584c
shoccho <sfb.shoccho@gmail.com>
2024-08-27 01:06:36 +0600

    refactored ApiCaller class

--- src/main/java/org/example/apiCaller/ApiCaller.java
+++ src/main/java/org/example/apiCaller/ApiCaller.java
@@ -9,76 +9,90 @@ import java.util.Iterator;
 
 public class ApiCaller {
     private final State state;
+    private final OkHttpClient client;
 
     public ApiCaller(State state) {
         this.state = state;
+        client = new OkHttpClient();
     }
 
     public ApiResponse callApi() throws IOException {
         String url = this.state.getUrl();
-        String method = this.state.getMethod();
-        if (method.equalsIgnoreCase("get")) {
-            JSONObject queries = this.state.getState("queries");
-            StringBuilder queryString = new StringBuilder();
-            Iterator<String> keys = queries.keys();
+        String method = this.state.getMethod().toLowerCase();
 
-            while (keys.hasNext()) {
-                String key = keys.next();
-                JSONObject child = new JSONObject(queries.getString(key));
-
-                if (!queryString.isEmpty()) {
-                    queryString.append("&");
-                }
-                String childKey = child.keys().next();
-
-                queryString.append(childKey).append("=").append(child.getString(childKey));
-            }
-            Request request = new Request.Builder()
-                    .url(url + "?" + queryString)
-                    .build();
-
-            OkHttpClient client = new OkHttpClient();
-            System.out.println(request.url());
-            Call call = client.newCall(request);
-            try {
-                Response response = call.execute();
-                return new ApiResponse(response.code(), response.body().string());
-            } catch (IOException e) {
-                throw new IOException(e);
-            }
-        } else if (method.equalsIgnoreCase("post")) {
-            JSONObject body = this.state.getState("body");
-            Iterator<String> keys = body.keys();
-            StringBuilder bodyJson = new StringBuilder();
-            while (keys.hasNext()) {
-                String key = keys.next();
-                String value = body.getString(key);
-                if (!bodyJson.isEmpty()) {
-                    bodyJson.append(",");
-                }
-                bodyJson.append(value);
-            }
-            final RequestBody requestBody = RequestBody.create(
-                    MediaType.parse("application/json"), bodyJson.toString());
-
-            Request request = new Request.Builder()
-                    .url(url)
-                    .method(method.toUpperCase(), requestBody)
-                    .build();
-
-            OkHttpClient client = new OkHttpClient();
-            System.out.println(request.url());
-            Call call = client.newCall(request);
-            try {
-                Response response = call.execute();
-                return new ApiResponse(response.code(), response.body().string());
-            } catch (IOException e) {
-                throw new IOException(e);
-            }
+        if (method.equals("get")) {
+            return executeGetRequest(url);
+        } else if (method.equals("post")) {
+            return executePostRequest(url);
         } else {
-            throw new Error("UnImplemented" + method);
+            throw new UnsupportedOperationException("Unsupported method: " + method);
         }
-
     }
 
+    private ApiResponse executeGetRequest(String url) throws IOException {
+        String queryString = buildQueryString(this.state.getState("queries"));
+        Request request = new Request.Builder()
+                .url(url + "?" + queryString)
+                .build();
+
+        return executeRequest(request);
+    }
+
+    private ApiResponse executePostRequest(String url) throws IOException {
+        String bodyJson = buildBodyJson(this.state.getState("body"));
+        RequestBody requestBody = RequestBody.create(
+                bodyJson, MediaType.parse("application/json"));
+
+        Request request = new Request.Builder()
+                .url(url)
+                .post(requestBody)
+                .build();
+
+        return executeRequest(request);
+    }
+
+    private String buildQueryString(JSONObject queries) {
+        StringBuilder queryString = new StringBuilder();
+        Iterator<String> keys = queries.keys();
+
+        while (keys.hasNext()) {
+            String key = keys.next();
+            JSONObject child = new JSONObject(queries.getString(key));
+            String childKey = child.keys().next();
+            String value = child.getString(childKey);
+
+            if (queryString.length() > 0) {
+                queryString.append("&");
+            }
+            queryString.append(childKey).append("=").append(value);
+        }
+
+        return queryString.toString();
+    }
+
+    private String buildBodyJson(JSONObject body) {
+
+        Iterator<String> keys = body.keys();
+        StringBuilder bodyJson = new StringBuilder();
+        while (keys.hasNext()) {
+            String key = keys.next();
+            String value = body.getString(key);
+            if (!bodyJson.isEmpty()) {
+                bodyJson.append(",");
+            }
+            bodyJson.append(value);
+        }
+        return bodyJson.toString();
+    }
+
+    private ApiResponse executeRequest(Request request) throws IOException {
+        System.out.println("Request URL: " + request.url());
+        try (Response response = client.newCall(request).execute()) {
+            if (response.body() != null) {
+                return new ApiResponse(response.code(), response.body().string());
+            } else {
+                return new ApiResponse(response.code(), "");
+            }
+        }
+    }
 }
